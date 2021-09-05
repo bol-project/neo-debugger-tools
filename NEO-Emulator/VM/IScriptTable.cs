bd0a336bf454cd4b99da8175536725da77f590ce
Charalampos Chomenidis <hampos@me.com>
2021-09-05 21:52:53 +0300

    Add Script Table dummy implementation

--- NEO-Emulator/VM/IScriptTable.cs
+++ NEO-Emulator/VM/IScriptTable.cs
@@ -4,4 +4,12 @@
     {
         byte[] GetScript(byte[] script_hash);
     }
+
+    public class ScriptTable : Neo.VM.IScriptTable
+    {
+        public byte[] GetScript(byte[] script_hash)
+        {
+            return new byte[0];
+        }
+    }
 }
